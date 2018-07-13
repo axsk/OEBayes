e1a428b2650c30e80084c3f2430af3a88d47e368
axsk <smartmind23@googlemail.com>
2018-07-13 13:28:36 +0200

    add cvreference function
    find best reference regularizer gamma for model m, data d from all gammas.

--- src/cv.jl
+++ src/cv.jl
@@ -2,14 +2,25 @@ import MLBase
 
 function cvscore(m, d, estimator, k=length(d))
     scores = MLBase.cross_validate(
-	i->estimator(d[i]),
-	(w,i)->datalikelihood(m, w, d[i]),
-	length(d),
-	MLBase.Kfold(length(d), k))
+        i->estimator(d[i]),
+        (w,i)->datalikelihood(m, w, d[i]),
+        length(d),
+        MLBase.Kfold(length(d), k))
     mean(scores)
 end
 
 function datalikelihood(m, w, d)
     L = likelihoodmat(m, d)
     logL(w,L)
+end
+
+# TODO: think about easier interface
+# i.e. give data, model, gammas
+
+function cvreference(m, d, gammas; k=length(d), c = OptConfig())
+    regs = [ReferenceRegularizer(m, gamma) for gamma in gammas]
+    cvs = [cvscore(m, data, d->ebprior(m, d, r, c), k) for r in regs]
+    i = indmax(cvs)
+    in(gammas[i], extrema(gammas)) && warn("extremal choice of gamma, consider extending range") 
+    regs[i]
 end
