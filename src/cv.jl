6dd65ea491fabacea28b24469e6089c62c196109
axsk <smartmind23@googlemail.com>
2018-07-13 13:49:50 +0200

    improve crossvalidation
    dont recompute jeffreysprior and likelihoodmatrices

--- src/cv.jl
+++ src/cv.jl
@@ -1,26 +1,18 @@
 import MLBase
 
-function cvscore(m, d, estimator, k=length(d))
-    scores = MLBase.cross_validate(
-        i->estimator(d[i]),
-        (w,i)->datalikelihood(m, w, d[i]),
-        length(d),
-        MLBase.Kfold(length(d), k))
+function cvscore(m, d, estimator::Function, k=length(d))
+    L = likelihoodmat(m, d)
+    score(w, i) = logL(w, L[:,i])   # logL(priorest_[-i] | data_[i])
+    scores = MLBase.cross_validate(i->estimator(d[i]), score, length(d), MLBase.Kfold(length(d), k))
     mean(scores)
 end
 
-function datalikelihood(m, w, d)
-    L = likelihoodmat(m, d)
-    logL(w,L)
-end
-
-# TODO: think about easier interface
-# i.e. give data, model, gammas
-
+" Given model m, data d and a list of gammas, return the (cv-)best regularizer"
 function cvreference(m, d, gammas; k=length(d), c = OptConfig())
-    regs = [ReferenceRegularizer(m, gamma) for gamma in gammas]
+    j = jeffreysprior(m)
+    regs = [ReferenceRegularizer(j, gamma) for gamma in gammas]
     cvs = [cvscore(m, data, d->ebprior(m, d, r, c), k) for r in regs]
     i = indmax(cvs)
     in(gammas[i], extrema(gammas)) && warn("extremal choice of gamma, consider extending range") 
-    regs[i]
+    regs[i], cvs
 end
